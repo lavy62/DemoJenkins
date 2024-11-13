f28f9004222ada1e9e30c79562262a2a9f9a3eb0
Admin <Admin@DESKTOP-UJRSTTQ>
2024-11-13 08:33:43 +0530

    pushed

---
+++ jenkinsdemo/src/test/java/contact/TocreateContactTest.java
@@ -0,0 +1,11 @@
+package contact;
+
+public class TocreateContactTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("created succesful");
+
+	}
+
+}
